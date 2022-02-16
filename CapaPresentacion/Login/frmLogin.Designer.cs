ae00fe91f29b123c94a747b2f6351120ff4b317d
Lnx <57012894+Lnx1408@users.noreply.github.com>
2022-02-16 14:55:47 -0500

    Capa Presentación.

---
+++ CapaPresentacion/Login/frmLogin.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CapaPresentacion.Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbRegistrarUsuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.Mesa_de_trabajo_3;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(421, 469);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbRegistrarUsuario
+            // 
+            this.lbRegistrarUsuario.AutoSize = true;
+            this.lbRegistrarUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(151)))), ((int)(((byte)(199)))));
+            this.lbRegistrarUsuario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbRegistrarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbRegistrarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRegistrarUsuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbRegistrarUsuario.Location = new System.Drawing.Point(237, 389);
+            this.lbRegistrarUsuario.Name = "lbRegistrarUsuario";
+            this.lbRegistrarUsuario.Size = new System.Drawing.Size(73, 17);
+            this.lbRegistrarUsuario.TabIndex = 6;
+            this.lbRegistrarUsuario.Text = "Regístrate";
+            this.lbRegistrarUsuario.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbRegistrarUsuario.Click += new System.EventHandler(this.lbRegistrarUsuario_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(151)))), ((int)(((byte)(199)))));
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(88, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "¿Eres usuario nuevo?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(165)))), ((int)(((byte)(23)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(71, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Iniciar sesión";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(151)))), ((int)(((byte)(199)))));
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtPassword.Location = new System.Drawing.Point(92, 255);
+            this.txtPassword.MaxLength = 15;
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(211, 21);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(151)))), ((int)(((byte)(199)))));
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtUser.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtUser.Location = new System.Drawing.Point(92, 175);
+            this.txtUser.MaxLength = 15;
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(211, 19);
+            this.txtUser.TabIndex = 7;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(421, 469);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lbRegistrarUsuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbRegistrarUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+    }
+}
---
+++ CapaPresentacion/Login/frmLogin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaPresentacion.Usuario;
+
+namespace CapaPresentacion.Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar = '*';
+
+        }
+
+        private SqlConnection abrirConexionSqlServer()
+        {
+            SqlConnection conexion = new SqlConnection();
+            try
+            {
+                String stringConexion = "server = localhost; database = LosRapidosSAbd; integrated security = true";
+                conexion.ConnectionString = stringConexion;
+                conexion.Open();
+                Console.WriteLine("Se ha abierto la conexión con la base de datos de Sql Server");
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se ha podido establecer conexión con la base de datos, error: " + e);
+            }
+            return conexion;
+        }
+
+        private SqlConnection cerrarConexionSqlServer(SqlConnection conexion)
+        {
+            try
+            {
+                conexion.Close();
+                Console.WriteLine("Se ha cerrado la conexión con la base de datos Sql Server");
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se ha podido cerrar la conexión con la base de datos Sql Server, error: " + e);
+            }
+            return conexion;
+        }
+
+        public bool login(string user, string password)
+        {
+            bool acceso=false;
+            try
+            {
+                SqlConnection conexion = abrirConexionSqlServer();
+                String cadena = ("SELECT NombreUsuario, password FROM Usuario WHERE NombreUsuario = '"+user+"' AND password = '"+password+"'");
+                SqlCommand comando = new SqlCommand(cadena, conexion);
+                SqlDataReader registros = comando.ExecuteReader();
+                if (registros.Read())
+                {
+                    frmMenu menu = new frmMenu();
+                    menu.Show();
+                    this.Hide();
+                    acceso = true;
+                }
+                else
+                {
+                    MessageBox.Show("usuario no encontrado");
+                }
+                cerrarConexionSqlServer(conexion);
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se pudo acceder a la base de dqatos\n" + e);
+            }
+
+            return acceso;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login(txtUser.Text, txtPassword.Text);
+        }
+
+        private void lbRegistrarUsuario_Click(object sender, EventArgs e)
+        {
+            frmRegistrarUsuario registrarUsuario = new frmRegistrarUsuario();
+            registrarUsuario.Show();
+        }
+    }
+}
