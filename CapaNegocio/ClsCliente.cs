0ada2713b3e5acdc91100ed76239004b7133766b
Arlette998 <58401108+Arlette998@users.noreply.github.com>
2022-02-17 12:29:17 -0500

    Update ClsCliente.cs
    Mensaje actualizado.

--- CapaNegocio/ClsCliente.cs
+++ CapaNegocio/ClsCliente.cs
@@ -66,7 +66,7 @@ namespace CapaNegocio
                 int t = Convert.ToInt32(comannd.ExecuteScalar()); // con esa linea executa el insert a la bd
 
                     baseDatos.cerrar_conexion(conexion);
-                msj = "Se han registrado de forma exitosa cada cambio (BY: Bethsaida)";
+                msj = "Se registro exitosamente sus datos (BY: Arlette Rama).";
             }
             catch (Exception ex)
             {
